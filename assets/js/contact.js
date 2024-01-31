a9b48e9a88ea29a492c82cc7224df77948afc162
Maria Espino <127848953+mgsetler@users.noreply.github.com>
2024-01-31 13:23:14 -0700

    Add files via upload
    added js files

---
+++ assets/js/contact.js
@@ -0,0 +1,37 @@
+/*=============== EMAIL JS ===============*/
+const contactForm = document.getElementById("contact-form"),
+	contactMessage = document.getElementById("contact-message");
+
+const sendEmail = (e) => {
+	e.preventDefault();
+
+	// serviceID - templateID - #form - publicKey
+	emailjs
+		.sendForm(
+			"service_w7kavvg",
+			"template_ro8itsk",
+			"#contact-form",
+			"QVoA937EgBMNiaSVq",
+		)
+		.then(
+			() => {
+				// Show sent message
+				contactMessage.textContent = "Message sent successfully ✅";
+
+				// Remove message after five seconds
+				setTimeout(() => {
+					contactMessage.textContent = "";
+				}, 5000);
+
+				// Clear input fields
+				contactForm.reset();
+			},
+			() => {
+				// Show error message
+				contactMessage.textContent =
+					"Message not sent (service error) ❌";
+			},
+		);
+};
+
+contactForm.addEventListener("submit", sendEmail);
